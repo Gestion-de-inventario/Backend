6f15a55cc949ce0d7e6dcfd41b7505191ae11178
javierUPAO <jaguilarb3@upao.edu.pe>
2026-05-25 17:48:54 -0500

    Feat docker config

--- backendcomedor/src/main/java/com/comedor/backend/infrastructure/config/EnvConfig.java
+++ backendcomedor/src/main/java/com/comedor/backend/infrastructure/config/EnvConfig.java
@@ -9,11 +9,5 @@ public class EnvConfig {
 
     @PostConstruct
     public void init() {
-        Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("RENIEC_TOKEN", dotenv.get("RENIEC_TOKEN"));
     }
 }
