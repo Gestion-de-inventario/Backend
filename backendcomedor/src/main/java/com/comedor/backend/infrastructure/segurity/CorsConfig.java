3d22003a4d1b56aaafd6076022b072918593a426
javierUPAO <jaguilarb3@upao.edu.pe>
2026-05-25 22:56:21 -0500

    fix: Url Frontend bloqueada

--- backendcomedor/src/main/java/com/comedor/backend/infrastructure/segurity/CorsConfig.java
+++ backendcomedor/src/main/java/com/comedor/backend/infrastructure/segurity/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200","https://comedorpopularluzdeisrael.web.app"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
